e61ed453ad979397cc4e6ffe4551676b5be39c6d
Zachary Reiss <zachary.reiss123@gmail.com>
2023-05-22 22:42:11 -0600

    Add splash screen - WIP app menu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'frontend';
+
+  
+  appTitle = 'SodaMix Time Manager';
+
+
+  navItems: MenuItem[] = [
+    {
+       label: 'Scheduler',
+       items: [
+          {
+             label: 'Schedule Time',
+             routerLink: '/scheduler',
+          },
+          {
+             label: 'Shift request',
+             routerLink: '/shiftRequest',
+          },
+          {
+             label: 'Manager',
+             routerLink: '/managerTools',
+          },
+       ],
+    },
+ ];
+;
+
+   constructor(
+    private _router: Router,
+  ) { }
+
+   ngOnInit(): void {   }
+
+   navigateTo(url: string): void {
+    this._router.navigate([url]);
+ }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
